4a1e46b599a990be88c1cf9167a78b1106d74d3e
darkmoon <fylz1125@163.com>
2019-06-01 20:04:08 +0800

    phong材质

--- assets/Script/Helloworld.ts
+++ assets/Script/Helloworld.ts
@@ -3,25 +3,28 @@ const {ccclass, property} = cc._decorator;
 @ccclass
 export default class Helloworld extends cc.Component {
 
+
     onLoad() {
+        
+        this.getComponent(cc.SkeletonAnimation);
         cc.loader.loadRes("rumba", function (err, sound) {
             cc.audioEngine.playMusic(sound, true);
         });
+        
     }
 
-
-    start() {
-        // cc.audioEngine.playMusic(this.bgm,true);
-
-    }
 
     playSamba() {
         let animation = this.getComponent(cc.Animation);
         animation.play("mixamo.com");
+        // this.node.runAction(cc.jumpBy(0.8, -400, 0, 300, 2));
+        
     }
 
     playMacarena() {
         let animation = this.getComponent(cc.Animation);
         animation.play("macarena");
     }
+
+
 }
